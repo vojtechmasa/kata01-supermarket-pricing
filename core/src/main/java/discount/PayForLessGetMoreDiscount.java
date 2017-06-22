5b93cf632da7a7bb0a7d802492b650011471dd9f
vojtechmasa <info@vojtechmasa.com>
2017-06-22 21:28:00 +0200

    Pay for Less Get More discount + tests

---
+++ core/src/main/java/discount/PayForLessGetMoreDiscount.java
@@ -0,0 +1,21 @@
+package discount;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
+public class PayForLessGetMoreDiscount implements Discount {
+  private final int getAmount;
+  private final int payAmount;
+
+  public PayForLessGetMoreDiscount(int getAmount, int payAmount) {
+    this.getAmount = getAmount;
+    this.payAmount = payAmount;
+  }
+
+  @Override
+  public double price(double unitPrice, double quantity) {
+    int eligibleForFree = ((int) quantity / getAmount) * (getAmount - payAmount);
+
+    return unitPrice * (quantity - eligibleForFree);
+  }
+}
---
+++ core/src/test/java/discount/PayForLessGetMorePriceDiscountShould.java
@@ -0,0 +1,46 @@
+package discount;
+
+import org.junit.Test;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@ParametersAreNonnullByDefault
+public class PayForLessGetMorePriceDiscountShould {
+  @Test
+  public void countCorrectly3for2Discount() {
+    //GIVEN
+    PayForLessGetMoreDiscount payForLessGetMoreDiscount = new PayForLessGetMoreDiscount(3, 2);
+
+    //WHEN 1
+    double actualPrice = payForLessGetMoreDiscount.price(10, 1);
+
+    //THEN 1
+    double expectedPrice = 10;
+    assertThat(actualPrice, is(equalTo(expectedPrice)));
+
+    //WHEN 2
+    actualPrice = payForLessGetMoreDiscount.price(10, 5);
+
+    //THEN 2
+    expectedPrice = 10 * 4;
+    assertThat(actualPrice, is(equalTo(expectedPrice)));
+
+    //WHEN 3
+    actualPrice = payForLessGetMoreDiscount.price(10, 6);
+
+    //THEN 3
+    expectedPrice = 10 * 4;
+    assertThat(actualPrice, is(equalTo(expectedPrice)));
+
+    //WHEN 4
+    actualPrice = payForLessGetMoreDiscount.price(10, 7);
+
+    //THEN 4
+    expectedPrice = 10 * 5;
+    assertThat(actualPrice, is(equalTo(expectedPrice)));
+  }
+}
